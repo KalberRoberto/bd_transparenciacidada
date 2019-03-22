ebfdee18a97505faf031b6c538c516f20d412968
KalberRoberto <kalber_roberto@hotmail.com>
2019-03-22 19:45:48 -0300

    modificaçao do script

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+rm /storage/neo4j/import/licitacao.txt
+rm /storage/neo4j/import/participante.txt
+
 wget -O licitacao.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Licitacao_Esfera_Municipal.txt.gz;
 wget -O participante.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Participantes_Licitacao_Esfera_Municipal.txt.gz;
 
 gunzip licitacao.txt.gz
 gunzip participante.txt.gz
+sed 's/"/ /g' licitacao.txt
 mv licitacao.txt /storage/neo4j/import/
-	mv participante.txt /storage/neo4j/import/
+mv participante.txt /storage/neo4j/import/
+
