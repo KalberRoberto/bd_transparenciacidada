0e8e20838c5af41f0d5580125cbd4ce689905b66
Kalber Roberto Pereira Silva <kalber.silva@virtus.ufcg.edu.br>
2019-05-28 15:26:15 -0300

    Atualizaçao de scripts do banco

--- kalbersc.sh
+++ kalbersc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm /storage/neo4j/import/licitacao.txt
-rm /storage/neo4j/import/participante.txt
+rm /var/lib/neo4j/import/licitacao.txt
+rm /var/lib/neo4j/import/participante.txt
 
 wget -O licitacao.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Licitacao_Esfera_Municipal.txt.gz;
 wget -O participante.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Propostas_Licitacao_Esfera_Municipal.txt.gz;
@@ -9,11 +9,11 @@ gunzip licitacao.txt.gz
 gunzip participante.txt.gz
 sed 's/"/ /g' licitacao.txt >> semaspas.txt
 mv semaspas.txt licitacao.txt
-mv licitacao.txt /storage/neo4j/import/
-mv participante.txt /storage/neo4j/import/
+mv licitacao.txt /var/lib/neo4j/import
+mv participante.txt /var/lib/neo4j/import
 
-cat cria_index_licitacao.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat cria_index_participante.cypher | cypher-shell -u neo4j -p tcctcc --format plain
+#cat cria_index_licitacao.cypher | cypher-shell -u neo4j -p tcctcc --format plain
+#cat cria_index_participante.cypher | cypher-shell -u neo4j -p tcctcc --format plain
 cat carrega_unidade_gestora.cypher | cypher-shell -u neo4j -p tcctcc --format plain
 cat carrega_licitacao.cypher | cypher-shell -u neo4j -p tcctcc --format plain
 cat carrega_participante.cypher | cypher-shell -u neo4j -p tcctcc --format plain
